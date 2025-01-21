a4e9dc45775d8a5ea3665f25109b15ef33b90e41
tejas3540 <masutagetejas3540@gmail.com>
2025-01-21 21:55:28 +0530

    palindrom.java

--- palindrom.java
+++ palindrom.java
@@ -1,19 +1,15 @@
 import java.util.*;
 public class palindrom {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int rev_n=0;
+	public static void main(String[] args) 
+  {
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int rev_n=0;
         while(n>0)
         {
             rev_n=rev_n*10+n%10;
             n=n/10;
         }
             System.out.println(rev_n);
-    if(rev_n == n){
-      System.out.println("It is Palndrom no.");
-	}
- else
-      System.out.println("It is Not Palndrom no.");
-}
-	
+  }
+}	
