5be332bb521ca5345604d60668f478ad79f4abdb
tejas3540 <masutagetejas3540@gmail.com>
2025-07-29 22:39:08 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,32 +1,23 @@
-package javadsa;
-
-public class BubbleSort {
-
-	public static void main(String[] args) {
-		int arr[]= {5,4,1,6,8,3};
-		int length= arr.length;
-		System.out.println("Unsorted");
-		for(int i=0;i<length; i++)
-		{
-			System.out.println(arr[i]+" ");
-		}
-		for(int i = 0; i<length;i++)
-		{
-			for(int j=1;j<length-i;j++)
-			{
-				if(arr[j-1]>arr[j])
-				{
-					int temp = arr[j];
-					arr[j]=arr[j-1];
-					arr[j-1] = temp;
-				}
-			}
-		}
-		for (int i =0; i<length; i++)
-		{
-			System.out.println("Sorted array:" +arr [i]+" ");
-		}
-
-	}
-
+public class bubblesort{
+public static void main(String [] args)
+  {
+      int [] arr ={9,4,5,1,2,4};
+      bubblesortex(arr);
+  }
+  public static void bubblesortex(int [] arr)
+    {
+      for (int i =0 ; i<arr.length-1; i++)
+        {
+          for(int j= 0; j<arr.length-i-1; j++)
+            {
+              if(arr[j]>arr[j+1])
+              {
+                int temp = arr[j];
+                arr[j]= arr[j+1];
+                arr[j+1]= temp;
+              }
+            }
+          System.out.println(Arrays.toString(arr));
+        }
+    }
 }
