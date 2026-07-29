c1a3dad65dd532ab877506d151abb4d9a608fa3a
Tejas masutage <masutagetejas3540@gmail.com>
2026-07-29 18:17:10 +0530

    Time: 1 ms (100%), Space: 47 MB (94.79%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,12 +1,25 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-      	int [] sorted = new int[nums.length];
-        int square = 0;
-	for (int i = 0; i < nums.length; i++) {
-		square = nums[i]*nums[i];
-		sorted[i] = square;
+        int [] sorted = new int[nums.length];
+    int lsquare = 0;
+	int rsquare=0;
+	int left = 0;
+	int right = nums.length-1;
+	int count = right;
+	while (left<=right)	{
+		lsquare = nums[left]*nums[left];
+		rsquare = nums[right]*nums[right];
+		
+			if(lsquare<rsquare) {
+			sorted[count]=rsquare;
+					right--;
+					count--;}
+		else {
+			sorted[count]= lsquare;
+			left++;
+			count--;
+		}
+		
 	}	
-	Arrays.sort(sorted);
-	return sorted;
-    }
+	return sorted;   }
 }
